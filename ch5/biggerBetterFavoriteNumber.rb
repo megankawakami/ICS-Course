51f4f4cb33568a9f8299d317901e787a8e55fa9f
megankawakami <megkaw1998@gmail.com>
2017-05-29 21:43:08 -0700

    week 3

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts "What's your favorite number?"
+number = gets.chomp.to_i
+better = number + 1
+puts 'Hmmm... well I think that ' + better.to_s + ' is a bigger and better favorite number!'
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,9 @@
+puts 'Hello there! What\'s your first name?'
+fname = gets.chomp
+puts 'What\'s your middle name?'
+mname = gets.chomp
+puts 'What\'s your last name?'
+lname = gets.chomp
+name = fname + ' ' + mname + ' ' + lname
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
